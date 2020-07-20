37de48e04a75a15c3fd603f227b25a2239f15060
Sayana <nisayana23@yandex.ru>
2020-07-20 19:17:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   # complexity.
   hash = {
     :railroads => {
-      :pieces => 4,
+      :pieces => "4",
       :rent_in_dollars => {},
       :names => {}
       }
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
   # complexity.
   hash = {
       :railroads => {
-      :pieces => 4,
+      :pieces => "4",
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
   # complexity.
   hash = {
       :railroads => {
-      :pieces => 4,
+      :pieces => "4",
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
